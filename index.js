4c11b096b26f9069b780b18f181205c371ea950e
黄佳佳 <jiajia.huang@71360.com>
2023-05-24 14:32:29 +0800

    增加 index

---
+++ index.js
@@ -0,0 +1,36 @@
+const axios = require('axios')
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const requestIp = require('request-ip')
+const services = require('./modules/services.js')
+const midjourney = require('./modules/midjourney')
+const app = express()
+const path = require("path");
+
+app.use(requestIp.mw())
+app.use(bodyParser.json({limit: '10mb'}))
+app.use(cors({origin: '*'}))
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+// 设置 EJS 模板引擎
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '.','views'));
+// 路由配置
+const loginRoutes = require('./modules/login')
+
+
+app.use('/', loginRoutes);
+
+
+axios.interceptors.request.use(request => {
+    request.maxContentLength = Infinity;
+    request.maxBodyLength = Infinity;
+    return request;
+})
+
+const port = process.env.PORT || 8000
+app.listen(port, async () => {
+    midjourney(app, await services(app))
+    console.log(`App running on *: http//:127.0.0.1:${port}`)
+})
